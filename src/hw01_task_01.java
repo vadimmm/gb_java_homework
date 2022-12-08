8b857382b16071d0c94c8733d4c482640fa891c1
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2022-12-08 11:50:49 +0300

    01 ok

--- src/hw01_task_01.java
+++ src/hw01_task_01.java
@@ -1,39 +1,52 @@
 //Вычислить n-ое треугольного число(сумма чисел от 1 до n)
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class hw01_task_01 {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_REVERSE = "\033[7m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
     public static void main(String[] args) {
-
-//        System.out.println(input_numb());
-//        System.out.println(triangular_number(input_numb()));
-//        System.out.println(triangular_number());
-//        System.out.println(triangular_number(n));
-//        System.out.println(init());
+        ShowTaskDescription();
+        System.out.println(triangular_number(GetIntNumb()));
 
     }
+    public static void ShowTaskDescription(){
+        String task = "Вычислить n-ое треугольного число(сумма чисел от 1 до n)";
+        String line = (ANSI_REVERSE + ' ' + ANSI_RESET).repeat(120);
+        System.out.println(line);
+        System.out.println("\n\t" + ANSI_YELLOW + task + ANSI_RESET +  "\n");
+        System.out.println(line);
+    }
 
-
-    public static void input_numb() {
-        System.out.print("Введите число 'n': ");
-        Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
-
-        if (n < 0) {
-            System.out.println("Ой! Вы случайно ввели отрицательное число " + n + " , так не будет произведён расчёт, но это не страшно!");
-            n *= -1;
-            System.out.println("Всё исправлено за вас!");
-        } else if (n == 0) {
-            System.out.println("Ой! Вы случайно ввели Н-И-Ч-Т-О, НОЛЬ, дырку от бублика!");
-            System.out.println("Сейчас вас вернёт на начальную точку!");
-            input_numb();
+    public static int GetIntNumb() {
+        Scanner numb = new Scanner(System.in);
+        int number;
+        while(true) {
+            try {
+                System.out.print("Пожалуйста, введите положительное число 'n': ");
+                number = numb.nextInt();
+                if(number <= 0) {
+                    throw new InputMismatchException();
+                }
+                break;
+            }
+            catch(InputMismatchException e) {
+                System.out.println(ANSI_RED + "Ой! Ошибка ввода! Попробуйте снова!" + ANSI_RESET);
+                numb.nextLine();
+            }
         }
-//        System.out.println(triangular_number(n));
-//        return n;
+        return number;
     }
 
-    public static void triangular_number(int n) {
-//        input_numb();
+    public static String triangular_number(int n) {
         int summ = 0;
         for (int i = 1; i <= n; i++) {
             summ += i;
@@ -41,6 +54,6 @@ public class hw01_task_01 {
         }
         System.out.print("\nОТВЕТ: ");
         System.out.print(n + "м числом треугольного числа является число " + summ);
-//        return "";
+        return "";
     }
 }
